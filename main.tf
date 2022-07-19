56ff3e03e851ac3d2869851e834475f8256a1b8d
Andrej Kislovskij <andrej.kislovskij@cast.ai>
2022-07-19 14:56:21 +0300

    Ignore helm app_version, revision, version attributes from TF state to manage upgrades only through controller actions or manually

--- main.tf
+++ main.tf
@@ -108,6 +108,10 @@ resource "helm_release" "castai_cluster_controller" {
   }
 
   depends_on = [helm_release.castai_agent]
+
+  lifecycle {
+    ignore_changes = [app_version, revision, version]
+  }
 }
 
 resource "helm_release" "castai_evictor" {
