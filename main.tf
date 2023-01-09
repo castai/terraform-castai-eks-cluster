d299af013c270ab408a1f3c08f2f26f57291cec6
anjmao <andzej.maciusovic@gmail.com>
2023-01-09 15:42:59 +0200

    Rename sec-agent

--- main.tf
+++ main.tf
@@ -235,12 +235,12 @@ resource "castai_autoscaler" "castai_autoscaler_policies" {
   depends_on = [helm_release.castai_agent, helm_release.castai_evictor]
 }
 
-resource "helm_release" "castai_sec_agent" {
+resource "helm_release" "castai_kvisor" {
   count = var.install_security_agent == true ? 1 : 0
 
-  name             = "castai-sec-agent"
+  name             = "castai-kvisor"
   repository       = "https://castai.github.io/helm-charts"
-  chart            = "castai-sec-agent"
+  chart            = "castai-kvisor"
   namespace        = "castai-agent"
   create_namespace = true
   cleanup_on_fail  = true
