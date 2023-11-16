85fad35c8f657b4ee8ef558bbc33aba101fb4393
Illia Litovchenko <illia@cast.ai>
2023-11-16 10:39:23 +0200

    KUBE-96: castai-agent replicas scaled to 2

--- main.tf
+++ main.tf
@@ -132,6 +132,11 @@ resource "helm_release" "castai_agent" {
   values  = var.agent_values
 
   set {
+    name  = "replicaCount"
+    value = "2"
+  }
+
+  set {
     name  = "provider"
     value = "eks"
   }
