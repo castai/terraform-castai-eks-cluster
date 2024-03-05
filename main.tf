d32d1725456ff9f64ae0ea7df8ff53867fbd0f2a
Matas Kaminskas <mataskam@gmail.com>
2024-03-05 15:00:48 +0200

    impl (#76)

--- main.tf
+++ main.tf
@@ -45,7 +45,7 @@ resource "castai_node_template" "this" {
 
   name             = try(each.value.name, each.key)
   is_default       = try(each.value.is_default, false)
-  is_enabled       = try(each.value.is_enabled, null)
+  is_enabled       = try(each.value.is_enabled, true)
   configuration_id = try(each.value.configuration_id, null)
   should_taint     = try(each.value.should_taint, true)
 
