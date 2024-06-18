abe704ec8d634614c09a95e0ef168cdcfe62ce36
eliAbdulrahman <elijah@cast.ai>
2024-06-18 11:30:28 -0700

    chore: remove default merge

--- main.tf
+++ main.tf
@@ -739,7 +739,7 @@ resource "helm_release" "castai_kvisor" {
   }
 
   dynamic "set" {
-    for_each = merge(var.kvisor_controller_extra_args.default, var.kvisor_controller_extra_args)
+    for_each = var.kvisor_controller_extra_args
     content {
       name  = "controller.extraArgs.${set.key}"
       value = set.value
@@ -781,7 +781,7 @@ resource "helm_release" "castai_kvisor_self_managed" {
   }
 
   dynamic "set" {
-    for_each = merge(var.kvisor_controller_extra_args.default, var.kvisor_controller_extra_args)
+    for_each = var.kvisor_controller_extra_args
     content {
       name  = "controller.extraArgs.${set.key}"
       value = set.value
