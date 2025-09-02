dbef9e401b83e8f4b531fd56e5f42d6a7475d722
Dima Baynak <dmytro@cast.ai>
2025-09-03 02:29:43 +0300

    add the self-managed version, even though i don't fully get what it is

--- main.tf
+++ main.tf
@@ -755,7 +755,35 @@ resource "helm_release" "castai_pod_mutator_self_managed" {
 }
 
 resource "helm_release" "castai_live" {
-  count = var.install_live && var.self_managed ? 1 : 0
+  count = var.install_live && (var.self_managed ? 0 : 1)
+
+  name             = "castai-live"
+  repository       = "https://castai.github.io/helm-charts"
+  chart            = "castai-live"
+  namespace        = "castai-agent"
+  create_namespace = true
+  cleanup_on_fail  = true
+  wait             = true
+
+  lifecycle {
+    ignore_changes = [version]
+  }
+
+  version = var.live_version
+  values  = var.live_values
+
+  set = concat(
+    var.install_live_cni ? [{ name = "castai-aws-vpc-cni.enabled", value = "true" }] : [],
+    local.set_cluster_id,
+    local.set_apiurl,
+    local.set_sensitive_apikey,
+  )
+
+  depends_on = [helm_release.castai_agent]
+}
+
+resource "helm_release" "castai_live_self_managed" {
+  count = var.install_live && (var.self_managed ? 1 : 0)
 
   name             = "castai-live"
   repository       = "https://castai.github.io/helm-charts"
@@ -775,7 +803,7 @@ resource "helm_release" "castai_live" {
     local.set_sensitive_apikey,
   )
 
-  depends_on = [helm_release.castai_agent, helm_release.castai_cluster_controller]
+  depends_on = [helm_release.castai_agent]
 }
 
 resource "castai_autoscaler" "castai_autoscaler_policies" {
