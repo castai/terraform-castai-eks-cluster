5e018cf5588a416730fdb1fee6f01d5adf7e89e0
Tadeuš Varnas <varnastadeus@gmail.com>
2025-11-12 11:59:10 +0200

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,6 @@
 output "cluster_id" {
   value       = castai_eks_cluster.my_castai_cluster.id
   description = "CAST AI cluster id, which can be used for accessing cluster data using API"
-  sensitive   = true
 }
 
 output "organization_id" {
