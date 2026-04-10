75102baf58a1bb48debbdb6dac93d48340ca0ccc
Valentyna Bukhalova <valentyna@cast.ai>
2026-04-10 13:40:45 +0200

    fix: default overwrite_existing_helm_releases to false

--- variables.tf
+++ variables.tf
@@ -380,6 +380,6 @@ variable "install_helm_apps" {
 
 variable "overwrite_existing_helm_releases" {
   type        = bool
-  default     = true
+  default     = false
   description = "Optional flag to control upgrade_install on all CAST AI Helm releases. When set to true, Helm will upgrade an existing release instead of failing with 'cannot re-use a name that is still in use'."
 }
